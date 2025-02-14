833be4578f6b820dec6c2721f6cde0a34fc34673
Tanmoy <talukdartanmay116@gmail.com>
2025-02-15 02:56:19 +0530

    Upload Feature Testing

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('imageUpload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const previewImg = document.getElementById('previewImg');
+            previewImg.src = e.target.result;
+            previewImg.style.display = "block"; // Show image
+        };
+        reader.readAsDataURL(file);
+    }
+});
